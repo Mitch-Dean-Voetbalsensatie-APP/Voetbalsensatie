598eb99316ad7774f2bf15ee0d1307de51ffdb4d
MW155836 <mitch_walravens@MitchWalravens.local>
2016-03-02 15:16:08 +0100

    scores

--- myApp/www/templates/playlist.php
+++ myApp/www/templates/playlist.php
@@ -2,7 +2,7 @@
   <ion-content>
     <h1>nfefwfe</h1>
          <?php
-    $uri = 'http://api.football-data.org/v1/soccerseasons/398/fixtures?matchday=20';
+    $uri = 'http://api.football-data.org/v1/soccerseasons/398/fixtures?matchday=28';
     $reqPrefs['http']['method'] = 'GET';
     $reqPrefs['http']['header'] = 'X-Auth-Token:68e5abb8860b467dba8e4c28f41ab20c';
     $stream_context = stream_context_create($reqPrefs);
@@ -19,10 +19,13 @@
   { ?> <tr> 
   <td><?php echo $key['homeTeamName']; ?></td> 
   <td><?php echo $key['awayTeamName']; ?></td> 
+  <td><?php echo $key['result']['goalsHomeTeam']; ?></td> 
+  <td><?php echo $key['result']['goalsAwayTeam']; ?></td> 
     <?php foreach($key['result'] as $values)
       { ?>
       <td><?php echo $values['goalsHomeTeam']; ?></td> 
       <td><?php echo $values['goalsAwayTeam']; ?></td> 
+
 </tr> <?php }}   ?> 
 </table>
 
