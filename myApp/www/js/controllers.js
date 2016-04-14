c959c9b836d395e7e3fc3781d136e06f1501db5b
MW155836 <mitch_walravens@MitchWalravens.local>
2016-04-14 09:05:55 +0200

    controller

--- myApp/www/js/controllers.js
+++ myApp/www/js/controllers.js
@@ -87,21 +87,19 @@ angular.module('starter.controllers', [])
 		var ref = new Firebase("https://sensatie.firebaseio.com");
 		ref.onAuth(function(authData) {
 			if (authData && isNewUser) {
-			// save the user's profile into the database so we can list users,
-			// use them in Security and Firebase Rules, and show profiles
 			ref.child("users").child(authData.uid).set({
 			provider: authData.provider,
-			name: getName(authData)
+			name: getName(authData),
+			premiereleague:true,
+			spanishleague:true
 			});
 		}
 		});
-		// find a suitable name based on the meta info given by each provider
+
 		function getName(authData) {
 			switch(authData.provider) {
 				case 'password':
 					return authData.password.email.replace(/@.*/, '');
-				case 'twitter':
-					return authData.twitter.displayName;
 				case 'facebook':
 					return authData.facebook.displayName;
 		}
